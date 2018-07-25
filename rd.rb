9f0682f72a832bddaa11b812212c4cd10c37b372
elizatu <eliza.turco@gmail.com>
2018-07-25 18:56:27 +0000

    automatically backed up by learn

---
+++ rd.rb
@@ -0,0 +1,14 @@
+loop do 
+  puts "Now watch me whip"
+  puts "Watch me nae nae"
+  puts "Now watch me whip, whip"
+  puts "Watch me nae nae"
+  puts "Ooooh, watch me, watch me"
+  puts "Ooooh, watch me, watch me"
+  puts "Ooooh, watch me, watch me"
+  break
+end
+
+puts "Do the stanky leg"
+puts "Do the stanky leg"
+exit
